0467831ec5bbb51bee687c28ad8ade07120b5a7a
ahmed gamel <ahmeddeg7@gmail.com>
2025-02-03 08:38:11 +0900

    feat(user.service): add method to create new users

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createUser(user: User, password: { hash: string; salt: string }) {
+    return this.prisma.user.create({
+      data: { ...user, passwords: { create: { ...password } } },
+    });
+  }
+}
