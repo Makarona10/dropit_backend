d7403c45d83bad5feb00b0d63b2ebb8a513d3569
ahmed gamel <ahmeddeg7@gmail.com>
2025-02-03 08:43:22 +0900

    feat(auth.controller): add register method

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { PasswordService } from 'src/password/password.service';
+import { UserService } from 'src/user/user.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(
+    private readonly passwordService: PasswordService,
+    private readonly userService: UserService,
+  ) {}
+
+  @Post('register')
+  async register(@Body() body: any) {
+    const { password, ...user } = body;
+    const hashAndSaltObj = await this.passwordService.hashPassword(password);
+    return this.userService.createUser(user, hashAndSaltObj);
+  }
+}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UserModule } from 'src/user/user.module';
+import { PasswordModule } from 'src/password/password.module';
+import { PasswordService } from 'src/password/password.service';
+import { UserService } from 'src/user/user.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController]
+  imports: [UserModule, PasswordModule,PrismaModule],
+  providers: [AuthService, PasswordService, UserService,],
+  controllers: [AuthController],
 })
 export class AuthModule {}
