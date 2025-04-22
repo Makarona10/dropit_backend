b8e36c3156c868bd5a666fa74daddc50d9773987
Makarona10 <ahmedneyssi@gmail.com>
2025-04-22 22:43:43 +0200

    check point for changing the work environment

---
+++ src/storage-quota/storage-quota.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { StorageQuotaController } from './storage-quota.controller';
+import { StorageQuotaService } from './storage-quota.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Module({
+  imports: [PrismaModule],
+  controllers: [StorageQuotaController],
+  providers: [StorageQuotaService, PrismaService],
+})
+export class StorageQuotaModule {}
